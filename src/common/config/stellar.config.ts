b31967eb9d47f6d602d382903b88c17e0ff46716
josean7link <josean7link@gmail.com>
2026-06-15 19:33:53 -0400

    (feat) update stellar config file

--- src/common/config/stellar.config.ts
+++ src/common/config/stellar.config.ts
@@ -20,20 +20,16 @@ export default registerAs('stellarConfig', () => ({
   prosper_treasury_secret_prod: process.env.PROSPER_TREASURY_SECRET_PROD,
   mainnet_url: 'https://horizon.stellar.org',
   smart_contract_admin: process.env.SMART_CONTRACT_ADMIN,
-  smart_contract_secret:
-    'ABDql6FIrNFRY2AO:0q+R0CdwNo0ANvDJD9h6VQ==:hxguhtbEjf5GvgYiVJOGMpXXktZsZUjReUU2IXTprKQXFTcABqB5I7l7XKpQtkfiUWn1NFhNVGo=', //process.env.SMART_CONTRACT_SECRET,
+  smart_contract_secret: process.env.SMART_CONTRACT_SECRET,
   soroban_fee: '3000000',
   soroban_url: 'https://mainnet.sorobanrpc.com',
   starting_balance: '3',
   testnet_url: 'https://horizon-testnet.stellar.org',
   timeout: 1800,
-  token_contract_address_usdc:
-    'CA7AUEOZT3RGFJX37VTNLVRRZ57SFEQWRDNRASLLX5TYOQY3FJ6AFZMJ', //process.env.TOKEN_CONTRACT_ADDRESS_USDCp,
-  token_contract_address_arsa:
-    'CACQBLU74W2S7JEG6M6EMBULSL3YWMPLPINSDYXGA2RZPZRZPYB5KB4U', //process.env.TOKEN_CONTRACT_ADDRESS_ARSAp,
+  token_contract_address_usdc: process.env.TOKEN_CONTRACT_ADDRESS_USDCp,
+  token_contract_address_arsa: process.env.TOKEN_CONTRACT_ADDRESS_ARSAp,
   usdc_issuer_address: USDC_ISSUER_ADDRESS,
   usdc_issuer_address_prod: USDC_ISSUER_ADDRESS_PROD,
   xlm_wallet_public: process.env.XLM_WALLET_PUBLIC,
-  xlm_wallet_secret:
-    'n8VkCaGnsKmBmdbK:1rATEJ3NWZrxkYbRPlBv/A==:Ui2CyViz7RDDUBHv8Oj+xHkj5kABpKaTyVCh8xPZgl/CkVP+sfiKj0cd8jWOBksq3wOaAzS0xkQ=', //process.env.XLM_WALLET_SECRET,
+  xlm_wallet_secret: process.env.XLM_WALLET_SECRET,
 }));
